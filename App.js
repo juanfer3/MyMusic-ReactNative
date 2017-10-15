7b630a947695d9ce572283bdb7ae5792c6bc8fba
juanfer3 <jfvilladiego2@misena.edu.co>
2017-10-15 12:01:44 -0500

    contenedores separados y lista con varios items

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import ArtistBox from './ArtistBox';
 
 const instructions = Platform.select({
   ios: 'hola juanfer Press Cmd+R to reload,\n' +
@@ -30,30 +31,19 @@ const instructions = Platform.select({
 
 export default class App extends Component<{}> {
   render() {
-
-    const imagen='https://lastfm-img2.akamaized.net/i/u/300x300/84141376f72f1a6a43a9d8ea1c695ab1.jpg'
-    const name = 'Andy Shouf'
-    const like = 200
-    const comment = 150
+    const artist={
+      imagen:'https://lastfm-img2.akamaized.net/i/u/300x300/84141376f72f1a6a43a9d8ea1c695ab1.jpg',
+      name: 'Andy Shouf',
+      like: 205,
+      comment: 150
+   }
     return (
       <View style={styles.container}>
-        <View style={styles.artistBox}>
-          <Image style={styles.image} source={{uri:imagen}} />
-          <View style={styles.info}>
-            <Text style={styles.name}>{name}</Text>
-            <View style={styles.row}>
-              <View style={styles.iconsContent}>
-                <Icon name="favorite-border" size={30} color="ligthgray"/>
-                <Text style={styles.count}>{like}</Text>
-              </View>
-              <View style={styles.iconsContent}>
-                <Icon name="forum" size={30} color="ligthgray"/>
-                <Text style={styles.count}>{comment}</Text>
-              </View>
-            </View>
-          </View>
+        <ArtistBox artist={artist}/>
+        <ArtistBox artist={artist}/>
+        <ArtistBox artist={artist}/>
+        <ArtistBox artist={artist}/>
 
-        </View>
       </View>
     );
   }
---
+++ ArtistBox.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  Modal,
+  Alert,
+  Image
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class ArtistBox extends Component<{}> {
+  render() {
+    //console.warn('el artista', this.props.artist.name);
+
+    const{name,like,comment,imagen}=this.props.artist
+    return (
+
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={{uri:imagen}} />
+          <View style={styles.info}>
+            <Text style={styles.name}>{name}</Text>
+            <View style={styles.row}>
+              <View style={styles.iconsContent}>
+                <Icon name="favorite-border" size={30} color="ligthgray"/>
+                <Text style={styles.count}>{like}</Text>
+              </View>
+              <View style={styles.iconsContent}>
+                <Icon name="forum" size={30} color="ligthgray"/>
+                <Text style={styles.count}>{comment}</Text>
+              </View>
+            </View>
+          </View>
+
+        </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  artistBox: {
+    margin: 5,
+    elevation: 2,
+    backgroundColor: '#b2dfdb',
+    flexDirection: 'row',
+    shadowColor: 'black',
+    shadowOpacity: .3,
+    shadowOffset:{
+      height:1,
+      width:-2,
+    },
+
+
+  },
+  image: {
+    height:150,
+    width: 150,
+  },
+  name: {
+
+    fontSize: 20,
+    marginTop: 10,
+
+  },
+  info:{
+    flex:1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  row:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 30,
+    marginTop: 15,
+  },
+  iconsContent:{
+    flex: 1,
+    alignItems: 'center'
+  },
+  count: {
+    color: '#424242'
+  }
+
+});
